e883d7bc3e4ed6d68b6a70a1ae81c26915d52a76
yazmacinakipali <nakipaliyazmaci@gmail.com>
2023-01-13 01:54:24 +0300

    first commit
    first commit for product service

---
+++ product-service/src/test/java/com/sbmc/nali/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmc.nali.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
